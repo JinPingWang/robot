e447646129631bd4b97649e35fd1c19f9acdfbd7
jinpingwang <wangjinping2014@qq.com>
2016-07-11 09:15:06 +0800

    modify SemanticNetRobot

--- src/main/java/robot/SemanticNetRobot.java
+++ src/main/java/robot/SemanticNetRobot.java
@@ -60,7 +60,13 @@ public class SemanticNetRobot implements Robot {
 		/**
 		 * 三：调用程强机器学习接口，将模糊答案对象传递给这个接口，得到最优答案
 		 */
-		String result = BestAnswer.getBestAnswerString(questionBean, answerBeans);
+		String result = "请问您的联系方式是？因为我这边是在线客服 信息掌握的不是很全面 可以安排专业顾问给您详细推荐一下";
+		if(answerBeans.size() == 0){
+			return result; 
+		}
+		else{
+			result = BestAnswer.getBestAnswerString(questionBean, answerBeans);
+		}
 			
 		/**
 		 * 四：如果最优答案为null，则调用刘爽黄为的主义网络，构造答案
